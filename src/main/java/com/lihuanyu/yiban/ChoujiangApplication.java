8065d9fb652f65484ebfeaf317db8ef31efffda3
skyADMIN <546485299@qq.com>
2016-02-03 10:14:15 +0800

    spring init

---
+++ src/main/java/com/lihuanyu/yiban/ChoujiangApplication.java
@@ -0,0 +1,12 @@
+package com.lihuanyu.yiban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoujiangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoujiangApplication.class, args);
+	}
+}
