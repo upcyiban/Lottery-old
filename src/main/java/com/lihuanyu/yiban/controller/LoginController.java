41a72938013b43e2dc0765a37a69a695cd54b1e0
skyADMIN <546485299@qq.com>
2016-02-25 21:18:48 +0800

    前端基本完成

--- src/main/java/com/lihuanyu/yiban/controller/LoginController.java
+++ src/main/java/com/lihuanyu/yiban/controller/LoginController.java
@@ -11,9 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
