1c0bb7894c30ce732d49fdcc0249a2e2d1700e82
skyADMIN <546485299@qq.com>
2016-02-28 09:05:17 +0800

    给一个开发用的配置类

---
+++ src/main/java/com/lihuanyu/yiban/config/DevConfig.java
@@ -0,0 +1,13 @@
+package com.lihuanyu.yiban.config;
+
+/**
+ * Created by skyADMIN on 16/2/28.
+ */
+public class DevConfig {
+    public static String client_id = "07f11a3f2773e24e";
+    public static String redirect_uri = "http://f.yiban.cn/iapp28401";
+    public static String AppSecret = "13e9bf919a795dee0f9d438c4dc8928d";
+
+    public static String adminUsername = "admin";
+    public static String adminPassword = "admin";
+}
