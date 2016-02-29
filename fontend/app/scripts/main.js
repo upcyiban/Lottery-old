519237ed9b33f17d40c7ef5d5e2db3432ebc75b0
skyADMIN <546485299@qq.com>
2016-02-29 19:37:48 +0800

    啦啦啦啦，前端

--- fontend/app/scripts/main.js
+++ fontend/app/scripts/main.js
@@ -1,1 +1,2 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+console.log('招聘前端!水平不限!'); // eslint-disable-line no-console
+console.log('联系方式,大活西南角易班办公室'); // eslint-disable-line no-console
