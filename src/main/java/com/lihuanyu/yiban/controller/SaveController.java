477dccb07ea4272eb31abbd034c942d7b13bfb7c
skyADMIN <546485299@qq.com>
2016-03-17 22:26:46 +0800

    完善了部分细节。

--- src/main/java/com/lihuanyu/yiban/controller/SaveController.java
+++ src/main/java/com/lihuanyu/yiban/controller/SaveController.java
@@ -35,30 +35,37 @@ public class SaveController {
             return "createresult";
         }
 
-        if(prize1<0||prize2<0||prize3<0||prize4<0){
-            return "false";
+        if(prize1<0||prize2<0||prize3<0||prize4<0||probability1<0||probability2<0||probability3<0||probability4<0){
+            model.addAttribute("result","出错了!");
+            model.addAttribute("word","奖品数量小于0或中奖率小于0");
+            return "createresult";
         }
+        try {
+            Timestamp timebegin = tranFromDatetimeLocal(lotterytimebegin);
+            Timestamp timeend = tranFromDatetimeLocal(lotterytimeend);
 
-        Timestamp timebegin = tranFromDatetimeLocal(lotterytimebegin);
-        Timestamp timeend = tranFromDatetimeLocal(lotterytimeend);
+            int yibanid = (int) httpSession.getAttribute("userid");
+            String yibanname = (String) httpSession.getAttribute("username");
 
-        int yibanid = (int) httpSession.getAttribute("userid");
-        String yibanname = (String) httpSession.getAttribute("username");
+            String result = createService.saveCreatorAndLottery(yibanid, yibanname, lotteryname, lotteryintro, timebegin, timeend, prize1, prize2, prize3, prize4, probability1, probability2, probability3, probability4);
 
-        String result = createService.saveCreatorAndLottery(yibanid,yibanname,lotteryname,lotteryintro,timebegin,timeend,prize1,prize2,prize3,prize4,probability1,probability2,probability3,probability4);
-
-        if (result.equals("success")){
-            String pageresult = "成功了!";
-            String word = "创建成功,等待管理员审核,急用可联系石大易班发展中心成员";
-            model.addAttribute("result",pageresult);
-            model.addAttribute("word",word);
-            return "createresult";
-        }else {
-            String pageresult = "失败了";
-            String word = "未知原因失败,再次尝试也不行请反馈到石大易班发展中心,等技术人员检查.";
-            model.addAttribute("result",pageresult);
-            model.addAttribute("word",word);
-            return "createresult";
+            if (result.equals("success")) {
+                String pageresult = "成功了!";
+                String word = "创建成功,等待管理员审核,急用可联系石大易班发展中心成员";
+                model.addAttribute("result", pageresult);
+                model.addAttribute("word", word);
+                return "createresult";
+            } else {
+                String pageresult = "失败了";
+                String word = "未知原因失败,再次尝试也不行请反馈到石大易班发展中心,等技术人员检查.";
+                model.addAttribute("result", pageresult);
+                model.addAttribute("word", word);
+                return "createresult";
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+            model.addAttribute("reason","请勿使用老旧浏览器如IE等填写表单,时间处是有格式要求的.");
+            return "error";
         }
     }
 
