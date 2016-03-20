e3332a69ea51cf19971c0e036b25609e8490b1a8
skyADMIN <546485299@qq.com>
2016-03-20 18:27:30 +0800

    更新配置信息

--- src/main/java/com/lihuanyu/yiban/controller/LotteryController.java
+++ src/main/java/com/lihuanyu/yiban/controller/LotteryController.java
@@ -1,18 +1,11 @@
 package com.lihuanyu.yiban.controller;
 
-import com.lihuanyu.yiban.model.LotteryList;
-import com.lihuanyu.yiban.model.LotteryListDao;
-import com.lihuanyu.yiban.model.PrizeList;
-import com.lihuanyu.yiban.model.PrizeListDao;
 import com.lihuanyu.yiban.services.LotteryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpSession;
-import java.sql.Timestamp;
-import java.util.Random;
 
 /**
  * Created by echao on 2016/2/25.
@@ -29,7 +22,7 @@ public class LotteryController {
     }
 
     @RequestMapping("/lotteryresult")
-    public String  lotteryResult(long id,Model model) {
+    public String lotteryResult(long id,Model model) {
        return lotteryService.dealLotteryResult(id,model);
     }
 }
